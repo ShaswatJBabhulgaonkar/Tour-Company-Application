43ba41372ff0beedb361d8d79173f5a298ce0613
ShaswatJBabhulgaonkar <56784741+ShaswatJBabhulgaonkar@users.noreply.github.com>
2019-12-01 20:03:34 -0800

    Create Customers.js

---
+++ Customers.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Head(){
+  return <header className="header"><h1>Customers : Not implemented yet</h1></header>;
+}
+
+function Body(){
+  return (
+    <section className="section">
+
+    </section>
+  );
+}
+
+function Customers(){
+  return <main className="main">
+            <Head />
+            <Body />
+        </main>;
+}
+
+export default Customers;
