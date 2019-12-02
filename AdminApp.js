b5f5152c07d7d8e0c3c1f28f0d4789eb92ea8a6e
ShaswatJBabhulgaonkar <56784741+ShaswatJBabhulgaonkar@users.noreply.github.com>
2019-12-01 20:00:38 -0800

    Update AdminApp.js

--- AdminApp.js
+++ AdminApp.js
@@ -1,36 +1,61 @@
-{
-  "virtualTours": [
-  {
-    "name": "Moon Tour",
-    "date": "Starting January 2020"
-  },
-  {
-    "name": "Galaxy Tour",
-    "date": "Starting February 2020"
-  },
-  {
-    "name": "Astronomy Tour",
-    "date": "Starting March 2020"
-  }],
-  "physicalTours": [
-  {
-    "name": "Rome Tour",
-    "date": "Starting April 2020"
-  },
-  {
-    "name": "Moscow Tour",
-    "date": "Starting May 2020"
-  },
-  {
-    "name": "Delhi Tour",
-    "date": "Starting June 2020"
-  },
-  {
-    "name": "London Tour",
-    "date": "Starting July 2020"
-  },
-  {
-    "name": "Lokseva World Tour",
-    "date": "Aug 2020 to Dec 2020"
-  }]
+import React from "react";
+import ReactDOM from "react-dom";
+import Home from "../guest/Home.js";
+import Customers from "./Customers.js";
+import About from "../guest/About.js";
+import AdminTours from "./AdminTour.js"
+import NotImplemented from "../guest/NotImplemented.js";
+
+class AdminApp extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {showing : "home"};
+  }
+
+  changeShow(state){
+      this.setState({
+        showing: state
+      });
+  }
+
+  logout(){
+    this.props.handleLogout();
+  }
+
+  render(){
+
+    let page;
+    if(this.state.showing === "home"){
+      page = <Home />;
+    }
+    else if(this.state.showing === "about"){
+      page = <About />;
+    }
+    else if(this.state.showing === "tours"){
+      page = <AdminTours />
+    }
+    else{
+      page = <NotImplemented state={this.state.showing} />;
+    }
+
+    return <div>
+            <div className="navigation">
+              <nav className="navigationBar">
+                <div className="icon"><span> Lokseva tours </span></div>
+                <div className="field">
+                  <ul className="role">
+                    <li className="item"><a className="atag" onClick={this.changeShow.bind(this, "tours")}>Tour Management</a></li>
+                    <li className="item"><a className="atag" onClick={this.changeShow.bind(this, "customers")}>Customer Management</a></li>
+                    <li className="item"><a className="atag" onClick={this.changeShow.bind(this, "about")}>About Us</a></li>
+                    <li className="item"><a className="atag" onClick={this.changeShow.bind(this, "home")}>Home</a></li>
+                    <li className="item"><a className="atag" onClick={this.logout.bind(this)}>Logout</a></li>
+                  </ul>
+                </div>
+              </nav>
+            </div>
+            {page}
+          </div>
+  }
 }
+
+export default AdminApp;
